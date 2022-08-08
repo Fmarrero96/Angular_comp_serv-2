9c826a017708bd2117ea743df02e2b36052969c1
Facundo Marrero <facu_marrero@hotmail.com>
2022-08-07 23:07:36 -0300

    Inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular_comp_serv_2';
+  imgParent = ''; //https://www.w3schools.com/howto/img_avatar.png
 }
